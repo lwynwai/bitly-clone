3a941c2765e050aa263a3afe082ac3155935019e
lwynwai <wynwai11@gmail.com>
2017-02-08 14:57:36 +0800

    commit

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,20 @@
+
+
 get '/' do
+  @urls = Url.all	
   erb :"static/index"
 end
+
+post '/urls' do 
+  url = Url.new
+  url.long_url = params[:long_url]
+  url.short_url = url.shorten
+  url.save
+end
+
+get '/:short_url' do
+  url = Url.find_by(short_url: params[:short_url]).long_url
+
+  redirect url
+end
+
